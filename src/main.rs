7600c79829ba79b022bc0ae688cbbcaf65a77b87
Victor Barrozo <victor.barrozo@crunchyroll.com>
2024-02-26 18:23:44 -0300

    change CLI args from string to Option so they support none values

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use std::io::{BufRead, BufReader, Write};
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    // name of the file to read. if none is provided, read from stdin
-    #[arg(long, default_value = "")]
-    file: String,
+    /// name of the file to read. if none = read from stdin
+    #[arg(long)]
+    file: Option<String>,
 
-    // where to output the results. if none is provided, write to stdout
-    #[arg(long, default_value = "")]
-    output: String,
+    /// where to output the results. if none = write to stdout
+    #[arg(long)]
+    output: Option<String>,
 }
 
 mod parser;
@@ -19,7 +19,7 @@ mod parser;
 fn main() {
     let args = Args::parse();
 
-    let lines = match read_input(&args.file) {
+    let lines = match read_input(args.file) {
         Ok(lines) => lines,
         Err(e) => {
             eprintln!("could not read input: {}", e);
@@ -35,48 +35,56 @@ fn main() {
         }
     };
 
+    // this last step is just for presentation purposes. the output is a map of games
+    // as presented on [the challenge](challenge.md)
     let mut games_map = std::collections::HashMap::new();
-    parsed
-        .iter()
-        .enumerate()
-        .for_each(|(i, game)| { games_map.insert(format!("game_{}", i), game); });
+    parsed.iter().enumerate().for_each(|(i, game)| {
+        games_map.insert(format!("game_{}", i), game);
+    });
 
-    write_output(&args.output, serde_json::to_string(&games_map).unwrap()).unwrap();
+    write_output(args.output, serde_json::to_string(&games_map).unwrap()).unwrap();
 }
 
-fn read_input(file: &str) -> Result<Vec<String>, String> {
-    if file == "" {
-        let reader = BufReader::new(std::io::stdin());
-        let lines = reader
-            .lines()
-            .filter(|e| e.is_ok())
-            .map(|e| e.unwrap())
-            .collect();
-        return Ok(lines);
+fn read_input(file: Option<String>) -> Result<Vec<String>, String> {
+    match file {
+        None => {
+            let reader = BufReader::new(std::io::stdin());
+            let lines = reader
+                .lines()
+                .filter(|e| e.is_ok())
+                .map(|e| e.unwrap())
+                .collect();
+            Ok(lines)
+        }
+        Some(file) => {
+            let f = match File::open(file) {
+                Ok(f) => f,
+                Err(e) => return Err(format!("could not open file: {}", e)),
+            };
+            let reader = BufReader::new(f).lines();
+            Ok(reader.filter(|e| e.is_ok()).map(|e| e.unwrap()).collect())
+        }
     }
-    let f = match File::open(file) {
-        Ok(f) => f,
-        Err(e) => return Err(format!("could not open file: {}", e)),
-    };
-    let reader = BufReader::new(f).lines();
-    Ok(reader.filter(|e| e.is_ok()).map(|e| e.unwrap()).collect())
 }
 
-fn write_output(output: &str, content: String) -> Result<(), String> {
-    if output == "" {
-        println!("{}", content);
-        return Ok(());
+fn write_output(output: Option<String>, content: String) -> Result<(), String> {
+    match output {
+        Some(out) => {
+            let mut f = match File::create(out) {
+                Ok(f) => f,
+                Err(e) => return Err(format!("could not create file: {}", e)),
+            };
+
+            match f.write(content.as_bytes()) {
+                Ok(_) => (),
+                Err(e) => return Err(format!("could not write to file: {}", e)),
+            }
+
+            Ok(())
+        }
+        None => {
+            println!("{}", content);
+            Ok(())
+        }
     }
-
-    let mut f = match File::create(output) {
-        Ok(f) => f,
-        Err(e) => return Err(format!("could not create file: {}", e)),
-    };
-
-    match f.write(content.as_bytes()) {
-        Ok(_) => (),
-        Err(e) => return Err(format!("could not write to file: {}", e)),
-    }
-
-    Ok(())
 }
